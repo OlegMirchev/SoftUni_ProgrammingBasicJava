83019672e178a434f482f648a083598292ce8532
OlegMirchev <olegmirchev@gmail.com>
2022-01-14 11:44:06 +0200

    Added homework

--- ProgrammingBasicWithJava/ProgrammingJavaJan2021LAB0901/HelloSoftUni.java
+++
@@ -1,7 +0,0 @@
-package ProgrammingBasic2021.TasksFromExercise.ProgrammingJavaJan2021LAB0901;
-
-public class HelloSoftUni {
-    public static void main(String[] args) {
-        System.out.println("Hello SoftUni");
-    }
-}
