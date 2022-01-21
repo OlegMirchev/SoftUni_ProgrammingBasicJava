4bde183998e074c8e5326af47c1b041e192e542a
OlegMirchev <olegmirchev@gmail.com>
2022-01-21 09:34:19 +0200

    Added homework

---
+++ ProgrammingBasicWithJava/ProgrammingJavaJan2021Ex1001/USDToBGN.java
@@ -0,0 +1,15 @@
+package ProgrammingBasic2021.TasksFromExercise.ProgrammingJavaJan2021Ex1001;
+
+import java.util.Scanner;
+
+public class USDToBGN {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double usd = Double.parseDouble(scanner.nextLine());
+
+        double bgn = usd * 1.79549;
+
+        System.out.println(bgn);
+    }
+}
