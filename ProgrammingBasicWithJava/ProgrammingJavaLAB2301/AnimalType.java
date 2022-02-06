a0d0776178a950880b17fcc879771e36d363f760
OlegMirchev <olegmirchev@gmail.com>
2022-02-06 20:44:29 +0200

    Added homework

---
+++ ProgrammingBasicWithJava/ProgrammingJavaLAB2301/AnimalType.java
@@ -0,0 +1,24 @@
+package ProgrammingBasic2021.TasksFromExercise.ProgrammingJavaJan2021LAB2301;
+
+import java.util.Scanner;
+
+public class AnimalType {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String animal = scanner.nextLine();
+
+        switch (animal) {
+            case "dog":
+                System.out.println("mammal");
+                break;
+            case "crocodile":
+            case "tortoise":
+            case "snake":
+                System.out.println("reptile");
+                break;
+            default:
+                System.out.println("unknown");
+                break;
+        }
+    }
+}
