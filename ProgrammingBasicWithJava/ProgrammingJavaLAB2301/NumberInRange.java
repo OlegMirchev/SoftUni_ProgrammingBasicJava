38bb3a1fdfcee9598f5ee7c6327783b2e47b35f0
OlegMirchev <olegmirchev@gmail.com>
2022-02-12 23:43:41 +0200

    Added homework

---
+++ ProgrammingBasicWithJava/ProgrammingJavaLAB2301/NumberInRange.java
@@ -0,0 +1,18 @@
+package ProgrammingBasic2021.TasksFromExercise.ProgrammingJavaJan2021LAB2301;
+
+import java.util.Scanner;
+
+public class NumberInRange {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = Integer.parseInt(scanner.nextLine());
+
+        if (number >= -100 && number <= 100 && number != 0) {
+            System.out.println("Yes");
+        }else {
+            System.out.println("No");
+        }
+
+    }
+}
+
