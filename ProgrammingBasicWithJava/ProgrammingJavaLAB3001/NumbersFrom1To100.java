0c8756c0764d9240a6a264f53a76cd926a3e8d86
OlegMirchev <olegmirchev@gmail.com>
2022-03-05 15:43:02 +0200

    Added homework

---
+++ ProgrammingBasicWithJava/ProgrammingJavaLAB3001/NumbersFrom1To100.java
@@ -0,0 +1,14 @@
+package ProgrammingBasic2021.TasksFromExercise.ProgrammingJavaJan2021LAB3001;
+
+public class NumbersFrom1To100 {
+    public static void main(String[] args) {
+
+
+        int i = 1;
+
+        for (; i <= 100; i++) {
+            System.out.println(i);
+
+        }
+    }
+}
