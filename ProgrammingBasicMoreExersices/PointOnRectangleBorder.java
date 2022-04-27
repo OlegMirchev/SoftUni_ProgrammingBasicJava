6abce9573aa49cb87fba1be74553cdf9b175f191
OlegMirchev <olegmirchev@gmail.com>
2022-04-27 13:27:52 +0300

    Added homework

---
+++ ProgrammingBasicMoreExersices/PointOnRectangleBorder.java
@@ -0,0 +1,28 @@
+package ProgrammingBasic2021.TasksFromExercise.PBMoreExersice;
+
+import java.util.Scanner;
+
+public class PointOnRectangleBorder {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double x1 = Double.parseDouble(scanner.nextLine());
+        double y1 = Double.parseDouble(scanner.nextLine());
+        double x2 = Double.parseDouble(scanner.nextLine());
+        double y2 = Double.parseDouble(scanner.nextLine());
+        double x = Double.parseDouble(scanner.nextLine());
+        double y = Double.parseDouble(scanner.nextLine());
+        boolean isValid = false;
+
+        if ((x == x1 || x == x2) && (y >= y1 && y <= y2)) {
+            isValid = true;
+        }
+        if ((y == y1 || y == y2) && (x >= x1 && x <= x2)) {
+            isValid = true;
+        }
+        if (isValid) {
+            System.out.println("Border");
+        }else {
+            System.out.println("Inside / Outside");
+        }
+    }
+}
